f288c65dd9d589749771f4e70035ea5e048e63ea
shivang-prabhu <68196435+shivang-prabhu@users.noreply.github.com>
2020-07-15 12:43:11 +0530

    Adding assignment 4

---
+++ assignment4(Day 1)/assignment4.js
@@ -0,0 +1,11 @@
+console.log("This is used for displaying messages on console ");
+console.error("This displays error in the console");
+console.warn("This is used for warning");
+ 
+var a={name:"Lucifer",place:"Hell"};
+var b={name:"GOD",  place:"Heaven"};
+var c={name:'Mortals', place:"Earth"};
+console.log(a);
+console.log(b);
+console.log({a,b,c});
+console.table({a,b,c});
