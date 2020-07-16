4e35b69511002418568223b86045f7b1b35a332f
shivang-prabhu <68196435+shivang-prabhu@users.noreply.github.com>
2020-07-16 11:32:21 +0530

    Adding assignment 8 for Day 2

---
+++ assignment8(Day 2)/assignment8.js
@@ -0,0 +1,12 @@
+let str="Good Morning";
+let str1=str.substring(0,4);
+console.log(str1);
+
+let str2=str.replace("Morning","Afternoon");
+console.log(str2);
+
+let str3= str.split("");
+console.log(str3);
+
+let str4= str.slice(0,4);
+console.log(str4);
