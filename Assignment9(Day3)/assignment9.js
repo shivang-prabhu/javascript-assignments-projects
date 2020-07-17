a37e38a391eb427fb90c755f5de4d564678bffa0
shivang-prabhu <68196435+shivang-prabhu@users.noreply.github.com>
2020-07-17 12:53:50 +0530

    Adding aassignment9 for Day 3

---
+++ Assignment9(Day3)/assignment9.js
@@ -0,0 +1,9 @@
+let num=prompt("Enter a number"); 
+if(num%2==1)
+{
+    console.log("Entered number is:" + num +" and the number is ODD");
+}
+else{
+    console.log("Entered number is:" + num +" and the number is EVEN");
+}
+
